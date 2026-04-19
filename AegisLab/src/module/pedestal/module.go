4d7f189e8807f94b763a75b1086c82b81baafc7c
lincyaw <814750204@qq.com>
2026-04-19 13:38:26 +0100

    phase-4: wire pedestal self-registration hooks (#38)

--- AegisLab/src/module/pedestal/module.go
+++ AegisLab/src/module/pedestal/module.go
@@ -10,6 +10,8 @@ var Module = fx.Module("pedestal",
 	fx.Provide(NewHandler),
 	fx.Provide(
 		fx.Annotate(Routes, fx.ResultTags(`group:"routes"`)),
+		fx.Annotate(Permissions, fx.ResultTags(`group:"permissions"`)),
+		fx.Annotate(RoleGrants, fx.ResultTags(`group:"role_grants"`)),
 		fx.Annotate(Migrations, fx.ResultTags(`group:"migrations"`)),
 	),
 )
