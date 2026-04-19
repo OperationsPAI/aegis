3a40ee2fcddabf67f37d152fd9dea6333918b48d
lincyaw <814750204@qq.com>
2026-04-19 11:21:19 +0100

    phase-4: migrate pedestal module for #38

--- AegisLab/src/module/pedestal/module.go
+++ AegisLab/src/module/pedestal/module.go
@@ -4,5 +4,12 @@ import "go.uber.org/fx"
 
 var Module = fx.Module("pedestal",
 	fx.Provide(NewRepository),
+	fx.Provide(NewService),
+	fx.Provide(AsHandlerService),
+	fx.Provide(AsReader),
 	fx.Provide(NewHandler),
+	fx.Provide(
+		fx.Annotate(Routes, fx.ResultTags(`group:"routes"`)),
+		fx.Annotate(Migrations, fx.ResultTags(`group:"migrations"`)),
+	),
 )
