9ec7c1a71a38a640d765a1c4862c4645c614dadf
lincyaw <814750204@qq.com>
2026-04-27 10:18:11 +0100

    test(subtask-2): cover real execute list response type (for issue #241)

--- AegisLab/src/cmd/aegisctl/cmd/execute_test.go
+++ AegisLab/src/cmd/aegisctl/cmd/execute_test.go
@@ -1,23 +1,16 @@
-package client
+package cmd
 
 import (
 	"encoding/json"
 	"testing"
+
+	"aegis/cmd/aegisctl/client"
 )
 
 func TestDecodeExecuteListResponse(t *testing.T) {
-	type executeListItem struct {
-		ID        int     `json:"id"`
-		Algorithm string  `json:"algorithm"`
-		Datapack  string  `json:"datapack"`
-		State     string  `json:"state"`
-		Duration  float64 `json:"duration"`
-		CreatedAt string  `json:"created_at"`
-	}
-
 	payload := `{"code":0,"message":"success","data":{"items":[{"id":101,"algorithm":"Traceback","datapack":"pair_diagnosis","state":"Success","duration":3,"created_at":"2026-04-26T08:12:24Z"}],"pagination":{"page":1,"size":20,"total":1,"total_pages":1}}}`
 
-	var resp APIResponse[PaginatedData[executeListItem]]
+	var resp client.APIResponse[client.PaginatedData[executeListItem]]
 	if err := json.Unmarshal([]byte(payload), &resp); err != nil {
 		t.Fatalf("decode execute list response: %v", err)
 	}
