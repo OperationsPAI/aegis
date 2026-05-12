40f13e0fb3d2ee79a4063dd157bd81ff4f971baf
lincyaw <814750204@qq.com>
2026-05-12 11:47:02 +0100

    refactor(injection): inline archive.go into datapack_store.go
    archive.go contained a single private function (packageDatapackDirectoryToZip)
    called only by DatapackStore.Package. Merge it directly into datapack_store.go
    to eliminate the one-function file and co-locate the zip packaging logic with
    the rest of the store.

--- AegisLab/src/core/domain/injection/datapack_store.go
+++ AegisLab/src/core/domain/injection/datapack_store.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
@@ -350,4 +351,34 @@ var validParquetFiles = map[string]bool{
 	"normal_traces.parquet":    true,
 	"normal_metrics.parquet":   true,
 	"normal_logs.parquet":      true,
+}
+
+func packageDatapackDirectoryToZip(zipWriter *zip.Writer, workDir string, excludeRules []utils.ExculdeRule) error {
+	err := filepath.WalkDir(workDir, func(path string, dir fs.DirEntry, err error) error {
+		if err != nil || dir.IsDir() {
+			return err
+		}
+
+		relPath, _ := filepath.Rel(workDir, path)
+		fullRelPath := filepath.Join(consts.DownloadFilename, filepath.Base(workDir), relPath)
+		fileName := filepath.Base(path)
+
+		for _, rule := range excludeRules {
+			if utils.MatchFile(fileName, rule) {
+				return nil
+			}
+		}
+
+		fileInfo, err := dir.Info()
+		if err != nil {
+			return err
+		}
+
+		return utils.AddToZip(zipWriter, fileInfo, path, filepath.ToSlash(fullRelPath))
+	})
+	if err != nil {
+		return fmt.Errorf("failed to package datapack directory %s: %w", filepath.Base(workDir), err)
+	}
+
+	return nil
 }
